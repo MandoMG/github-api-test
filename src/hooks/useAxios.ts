441c57e2249d8c42d9aa0b53d163e5fef5e29abf
Armando Meza <mando.mg.91@gmail.com>
2021-08-26 00:01:37 -0500

    Added hook to to handle commit data from response

---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useMemo, useState } from "react";
+import { CommitData } from "../types/commitDataTypes";
+
+const useAxios = () => {
+  const [githubData, setGithubData] = useState<CommitData[]>();
+
+  const fetchCommits = async () => {
+    const commitResponse = await axios.get('https://api.github.com/repos/mandomg/actions-test/commits');
+    setGithubData(commitResponse.data);
+  }
+
+  const commitList = useMemo(() => {
+    return !!githubData ? githubData?.map(commitData => {
+      return {
+        author: commitData.commit.author.name,
+        hash: commitData.sha,
+        message: commitData.commit.message
+      }
+    }) : [];
+  }, [githubData]);
+
+  useEffect(() => {
+    (async () => fetchCommits())()
+  }, []);
+
+  return { commitList };
+};
+
+export default useAxios;
