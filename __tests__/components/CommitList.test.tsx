b7e7bdc625ea7544ea89abdd60ad983000fb5fba
Armando Meza <mando.mg.91@gmail.com>
2021-08-27 23:36:12 -0500

    added component test with empty data

--- __tests__/components/CommitList.test.tsx
+++ __tests__/components/CommitList.test.tsx
@@ -2,32 +2,11 @@ import 'react-native';
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import CommitListComponent from '../../src/components/CommitList';
-
-const mockData = [
-  {
-    author: 'Armando Meza',
-    avatarUrl: 'https://someWebPage.com/assets/img/0110027/url0.jpg',
-    hash: '135a456ef15cf4156',
-    message: 'test commit #0'
-  },
-  {
-    author: 'Armando Meza',
-    avatarUrl: 'https://someWebPage.com/assets/img/0110027/url1.jpg',
-    hash: 'ef15cf41135a45656',
-    message: 'test commit #1'
-  },
-  {
-    author: 'Armando Meza',
-    avatarUrl: 'https://someWebPage.com/assets/img/0110027/url2.jpg',
-    hash: '2a411ef15565635a4',
-    message: 'test commit #2'
-  },
-]
-
+import mockCommitListData from '../__mocks__/MockCommitListData';
 
 describe('CommitList', () => {
   it('renders component', () => {
-    const wrapper = render(<CommitListComponent commitItemList={mockData} />);
+    const wrapper = render(<CommitListComponent commitItemList={mockCommitListData} />);
 
     expect(wrapper.queryByTestId(`commit-item-0-avatar-url`)).not.toBeNull();
     expect(wrapper.queryByTestId(`commit-item-0-header-text`)).not.toBeNull();
@@ -43,5 +22,24 @@ describe('CommitList', () => {
     expect(wrapper.queryByTestId(`commit-item-2-header-text`)).not.toBeNull();
     expect(wrapper.queryByTestId(`commit-item-2-message-text`)).not.toBeNull();
     expect(wrapper.queryByTestId(`commit-item-2-hash-text`)).not.toBeNull();
+  });
+
+  it('renders component - no data', () => {
+    const wrapper = render(<CommitListComponent commitItemList={[]} />);
+
+    expect(wrapper.queryByTestId(`commit-item-0-avatar-url`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-0-header-text`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-0-message-text`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-0-hash-text`)).toBeNull();
+
+    expect(wrapper.queryByTestId(`commit-item-1-avatar-url`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-1-header-text`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-1-message-text`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-1-hash-text`)).toBeNull();
+
+    expect(wrapper.queryByTestId(`commit-item-2-avatar-url`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-2-header-text`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-2-message-text`)).toBeNull();
+    expect(wrapper.queryByTestId(`commit-item-2-hash-text`)).toBeNull();
   });
 })
