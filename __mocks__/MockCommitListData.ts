e5404ab52c5ca97ac95ad5e388a8967faaa716f8
Armando Meza <mando.mg.91@gmail.com>
2021-08-28 00:58:01 -0500

    moved mocks to root folder

---
+++ __mocks__/MockCommitListData.ts
@@ -0,0 +1,22 @@
+const mockCommitListData = [
+  {
+    author: 'Armando Meza',
+    avatarUrl: 'https://someWebPage.com/assets/img/0110027/url0.jpg',
+    hash: '135a456ef15cf4156',
+    message: 'test commit #0'
+  },
+  {
+    author: 'Armando Meza',
+    avatarUrl: 'https://someWebPage.com/assets/img/0110027/url1.jpg',
+    hash: 'ef15cf41135a45656',
+    message: 'test commit #1'
+  },
+  {
+    author: 'Armando Meza',
+    avatarUrl: 'https://someWebPage.com/assets/img/0110027/url2.jpg',
+    hash: '2a411ef15565635a4',
+    message: 'test commit #2'
+  },
+]
+
+export default mockCommitListData;
